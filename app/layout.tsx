c5b70d4b33d7b06f92acadf5a8ef71c4ea5e0a9e
mallikaakash <mallik.aakash777@gmail.com>
2024-11-08 14:37:34 +0530

    setup nextjs-supabase template

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Kaizen",
+  description: "Improving your child 1% at a time.",
 };
 
 export default function RootLayout({
